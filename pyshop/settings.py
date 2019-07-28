52d23701350ce1eee6d3a75964128c51f93870df
nhon.tran <davidtranit80@gmail.com>
2019-07-28 16:57:05 +0900

    Use SQLlite

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -79,21 +79,21 @@ WSGI_APPLICATION = 'pyshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': '127.0.0.1',
-        'USER': 'root',
-        'PASSWORD': 'huongu83',
-        'NAME': 'david_db',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'HOST': '127.0.0.1',
+#         'USER': 'root',
+#         'PASSWORD': 'huongu83',
+#         'NAME': 'david_db',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
